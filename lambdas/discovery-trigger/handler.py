4e6ce86591ef4aad7412efb933e6c3180c21f602
Sean Quinlan <1011062+sbquinlan@users.noreply.github.com>
2023-04-10 11:16:23 -0700

    Add collection name to sf invoke

--- lambdas/discovery-trigger/handler.py
+++ lambdas/discovery-trigger/handler.py
@@ -1,16 +1,29 @@
 import os
 import json
-
+import re
 import boto3
+
+from uuid import uuid4
+
+INVALID_NAME_CHARS = re.compile("[^a-zA-Z0-9_-]")
+
+
+def filter_sfname(name):
+    if name is None:
+        return ""
+    return re.sub(INVALID_NAME_CHARS, "", name)
 
 
 def handler(event, context):
     STEP_FUNCTION_ARN = os.environ["STEP_FUNCTION_ARN"]
 
     event.pop("objects", None)
+    page = min(event.get("start_after", 1), 9999)
+    name = filter_sfname(event.get("collection", None))
 
     client = boto3.client("stepfunctions")
     client.start_execution(
+        name=f"{name[:38]}[{page:04d}]{str(uuid4())}",
         stateMachineArn=STEP_FUNCTION_ARN,
         input=json.dumps(event),
     )
