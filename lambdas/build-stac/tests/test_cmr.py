c3ee83085b3f231f1dd11b0124bbf7dd5ffa7240
emileten <e.tenezakis@gmail.com>
2023-04-21 09:54:12 +0900

    fix test, add unit test from from_cmr_link

--- lambdas/build-stac/tests/test_cmr.py
+++ lambdas/build-stac/tests/test_cmr.py
@@ -1,7 +1,5 @@
 from unittest.mock import patch
-
-import pytest
-from utils.stac import generate_stac_cmrevent
+from utils.stac import generate_stac_cmrevent, from_cmr_links
 
 
 def test_generate_stac_cmrevent(
@@ -9,12 +7,26 @@ def test_generate_stac_cmrevent(
 ):
     with patch("utils.stac.GranuleQuery.get") as mock_get:
         mock_get.return_value = [cmr_json_example]
-        with patch("utils.stac.get_assets_from_cmr") as mock_get_assets:
-            mock_get_assets.return_value = sample_assets
+        with patch("utils.stac.from_cmr_links") as mock_get_assets:
+            mock_get_assets.return_value = (
+                [],
+                sample_assets,
+            )  # empty links list for the puprpose of testing generate_stac_cmr_event
             result = generate_stac_cmrevent(cmr_multi_asset_sample_event)
-
             assert len(result.assets) == 3
             assert (
                 result.id
                 == "uavsar_AfriSAR_v1-coreg_fine_lopenp_14043_16008_140_009_160225_kz"
             )
+
+
+def test_from_cmr_links(cmr_json_example, cmr_multi_asset_sample_event):
+    with patch("utils.stac.generate_asset") as mock_generate_asset:
+        mock_generate_asset.return_value = "mocked_asset"
+        with patch("utils.stac.generate_link") as mock_generate_link:
+            mock_generate_link.return_value = "mocked_link"
+            links, assets = from_cmr_links(
+                cmr_json_example["links"], cmr_multi_asset_sample_event
+            )
+            assert len(links) == 1
+            assert len(assets) == 3
